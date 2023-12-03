ffa7443ea412cd250065e3751b52efe32d72ad37
Pruthviraj <expnit@outlook.com>
2023-12-03 15:49:13 +0530

    explained basic building block of C++, the basic structure of C++ program

---
+++ code/basic/cpp_program_basic_building_block.cpp
@@ -0,0 +1,17 @@
+
+// preprocessor directive that includes the content of iostream file here
+
+#include <iostream>
+
+// starting point of every C++ program that executes each statements written in its body
+int main()
+{ // starting main body
+
+    // std::cout inserts "hello world in C++" string into console. cout is console and endl is end of line
+
+    std::cout << "Hello world in C++" << std::endl; // statement 1
+
+    // function main returns zero to the operating system
+
+    return 0; // statement 2
+} // end of main's body
