56e07a81b6497528b9493f8c6d14995e760e4048
Pruthviraj <expnit@outlook.com>
2023-12-13 23:17:22 +0530

    addedsome comments

--- code/basic/global_variable_1.cpp
+++ code/basic/global_variable_1.cpp
@@ -25,5 +25,21 @@ int main()
 {
 
     sumTwoNumber();
+
+    // std::cout<<"First Number = " << firstNumber << std::endl;
+
+    // std::cout<<"Second Number = " << secondNumber <<std::endl;
+
+    // if i uncomment above lines the main function will generate an error
+
+    /*
+
+    variable firstNumber and secondNumber are locally created variable withint
+
+    sumTwoNumber() function so they are not known outside of its scope
+
+    main does not know anything about these variables
+
+    */
     return 0;
 }
