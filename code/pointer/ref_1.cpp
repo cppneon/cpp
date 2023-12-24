7b9903e3ceb29b50656409e740fb87b8301962a1
Pruthviraj <expnit@outlook.com>
2023-12-24 20:02:47 +0530

    delete repository

--- code/pointer/ref_1.cpp
+++ code/pointer/ref_1.cpp
@@ -4,7 +4,15 @@ int main()
 {
     int cppVersion = 23;
 
+    int array[4] = {0, 1, 2, 3};
+
+    int *ptrToCppVersion = &cppVersion;
+
     std::cout << "cppVersion variable is stored at : " << &cppVersion << std::endl;
+
+    std::cout << "ptrToCppVersion pointer : " << ptrToCppVersion << std::endl;
+
+    std::cout << "Value of cppVersion variable is : " << *ptrToCppVersion << std::endl;
 
     return 0;
 }
